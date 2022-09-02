cc5331ff88896bf89f4c04e072aba53ee5e8dbdd
matheusclmb <ccolombomatheus@gmail.com>
2022-09-03 00:03:14 +0100

    aditional testing

--- geniustest.py
+++ geniustest.py
@@ -11,8 +11,8 @@ load_dotenv()
 genius = genius.Genius(os.getenv('GENIUSTOKEN'))
 genius.remove_section_headers = True
 
-artist = genius.search_artist("Andy Shauf", max_songs=2, sort="title")
-print(artist.image_url)
+artist = genius.search_artist("Lil Peep", max_songs=2, sort="title")
+print(artist[0]["alternate_names"])
 
 
 
